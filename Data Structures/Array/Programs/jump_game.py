a0062e0e5f9c377c5eb8aeb792299a210e936a6e
chasesagar <chasesagar@gmail.com>
2022-01-29 13:44:31 +0530

    array game

--- Data Structures/Array/Programs/jump_game.py
+++ Data Structures/Array/Programs/jump_game.py
@@ -8,17 +8,20 @@ Input: arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
 Output: 3 (1-> 3 -> 9 -> 9)
 
 """
-
-from cmath import inf
-
-
 class ArrayGame:
     def __init__(self, arr: list = None, n: int = None):
         self.arr = arr
         self.n = n
 
     def minimum_jumps_to_reach_array_end_element(self):
-        self.minimum_jumps(self.arr, 0, self.n)
+        """
+        [A program to to find minimum jumps to reach array end using recursion]
+        Time complexity = O(n*n)
+        Auxillary Space = O(1)
+        Returns:
+            [int]: [Minimum jumps to reach self.arr[n] from self.arr[o]]
+        """
+        return self.minimum_jumps(self.arr, 0, self.n-1)
     
     @staticmethod
     def minimum_jumps(arr, low, high):
@@ -27,8 +30,15 @@ class ArrayGame:
         if arr[low] == 0: return float('inf')
 
         min = float('inf')
-        for i in range(low, high):
-            if i <= arr[low]:
-                jumps = ArrayGame.minimum_jumps(arr, arr[])
+        for i in range(low+1, high+1):
+            if i <= (low + arr[low] + 1):
+                jumps = ArrayGame.minimum_jumps(arr, i, high)
+                if jumps != float('inf') and jumps + 1 < min:
+                    min = jumps + 1
 
-        
+        return min
+
+# Driver program to test above function
+arr = [1, 3, 6, 3, 2, 3, 6, 8, 9, 5]
+res= ArrayGame(arr, len(arr))
+print('Minimum number of jumps to reach, end is', res.minimum_jumps_to_reach_array_end_element())
