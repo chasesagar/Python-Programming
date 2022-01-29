a7e7569c7abe688c270a5c01cbc23fea890c2d43
chasesagar <chasesagar@gmail.com>
2022-01-29 13:45:06 +0530

    array game

--- Data Structures/Array/Programs/jump_game.py
+++ Data Structures/Array/Programs/jump_game.py
@@ -8,6 +8,8 @@ Input: arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
 Output: 3 (1-> 3 -> 9 -> 9)
 
 """
+
+
 class ArrayGame:
     def __init__(self, arr: list = None, n: int = None):
         self.arr = arr
@@ -21,24 +23,27 @@ class ArrayGame:
         Returns:
             [int]: [Minimum jumps to reach self.arr[n] from self.arr[o]]
         """
-        return self.minimum_jumps(self.arr, 0, self.n-1)
-    
+        return self.minimum_jumps(self.arr, 0, self.n - 1)
+
     @staticmethod
     def minimum_jumps(arr, low, high):
         # base condition
-        if high == 1: return 0
-        if arr[low] == 0: return float('inf')
+        if high == 1:
+            return 0
+        if arr[low] == 0:
+            return float("inf")
 
-        min = float('inf')
-        for i in range(low+1, high+1):
+        min = float("inf")
+        for i in range(low + 1, high + 1):
             if i <= (low + arr[low] + 1):
                 jumps = ArrayGame.minimum_jumps(arr, i, high)
-                if jumps != float('inf') and jumps + 1 < min:
+                if jumps != float("inf") and jumps + 1 < min:
                     min = jumps + 1
 
         return min
 
+
 # Driver program to test above function
 arr = [1, 3, 6, 3, 2, 3, 6, 8, 9, 5]
-res= ArrayGame(arr, len(arr))
-print('Minimum number of jumps to reach, end is', res.minimum_jumps_to_reach_array_end_element())
+res = ArrayGame(arr, len(arr))
+print("Minimum number of jumps to reach, end is", res.minimum_jumps_to_reach_array_end_element())
